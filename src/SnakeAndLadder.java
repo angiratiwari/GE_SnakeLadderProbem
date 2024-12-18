5ad6fea18516a281cfe516494205878db9a91826
angiratiwari <angiratiwari1102@gmail.com>
2024-12-18 12:12:14 +0530

    Initial commit: Welcome to Snake and Ladder Game

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        // UC 1: Snake and Ladder game played with a single player at start position 0
+        System.out.println("Welcome to the Snake and Ladder Game!");
+        System.out.println("Single Player at Start Position 0");
+
+
+    }
+}
